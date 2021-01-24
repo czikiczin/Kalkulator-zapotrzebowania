f6663840bbea9c11dcd6fce3ea12a6fbc45119b7
czikiczin <jk055916@student.ath.edu.pl>
2021-01-24 19:03:04 +0100

    Aktualizacja login.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {LoginService} from './login.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
-  ngOnInit(): void {
+  onSubmit(form: NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.loginService.singin(email, password).subscribe(resData =>
+      {
+        console.log(resData);
+        this.router.navigate(['/']);
+      },
+      error => {
+        console.log(error);
+      });
+    form.reset();
   }
-
 }
