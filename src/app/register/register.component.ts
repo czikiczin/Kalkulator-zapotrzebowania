39e5872295f9bb949ddfa5e8744476db6fb48b9b
czikiczin <jk055916@student.ath.edu.pl>
2021-01-24 19:00:37 +0100

    Aktualizacja rejestracja.ts

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
+  onSubmit(form: NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.authService.signup(email, password).subscribe(resData =>
+    {
+      console.log(resData);
+      this.router.navigate(['/login']);
+    },
+      error => {
+        console.log(error);
+      });
+    form.reset();
   }
 
 }
