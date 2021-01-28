9872794f2b670955796ecfe66a9bf78d320bd313
Kamil Łukowicz <kamelghjk@gmail.com>
2021-01-28 17:57:02 +0100

    errory

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {LoginService} from './login.service';
 import {Router} from '@angular/router';
+import {throwError} from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -10,19 +11,33 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent {
 
+  errorMessage: string = null;
+  isLoading = false;
+
   constructor(private loginService: LoginService, private router: Router) { }
 
   onSubmit(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
     this.loginService.singin(email, password).subscribe(resData =>
       {
+        this.isLoading = false;
         console.log(resData);
         this.router.navigate(['/']);
       },
       error => {
-        console.log(error);
+        this.isLoading = false;
+        switch (error.error.error.message) {
+          case 'EMAIL_NOT_FOUND' :
+            this.errorMessage = 'Użytkownik z podanym mailem nie istnieje';
+            break;
+          case 'INVALID_PASSWORD' :
+            this.errorMessage = 'Nieprawidłowe hasło';
+            break;
+        }
+        return throwError(this.errorMessage);
       });
     form.reset();
   }
