6d5087d8147f485c364b152049cedda62318a789
Kamil Łukowicz <kamelghjk@gmail.com>
2021-01-28 17:45:40 +0100

    login odswiezanie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import {Component, OnInit} from '@angular/core';
+import {LoginService} from './login/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor() {
+export class AppComponent implements OnInit {
+  constructor(private loginService: LoginService) {
   }
   title = 'WebPageProject';
-  
+  ngOnInit() {
+    this.loginService.refreshLogin();
+  }
 }
