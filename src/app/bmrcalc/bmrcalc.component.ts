10089823ec25941d43c48841c21db5148bee1f01
Jakub Faruga <kuba-faruga@o2.pl>
2021-01-29 13:14:13 +0100

    KalkulatorBMR-zabezpieczenia .ts

--- src/app/bmrcalc/bmrcalc.component.ts
+++ src/app/bmrcalc/bmrcalc.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {timeout} from 'rxjs/operators';
 
 @Component({
   selector: 'app-bmrcalc',
@@ -16,15 +15,48 @@ export class BMRcalcComponent implements OnInit {
   activity;
   BMR;
   BMRact;
+  ifAgeOk = true;
+  ifWeightOk = true;
+  ifLenghtOk = true;
   constructor() { }
 
   ngOnInit(): void {
   }
 
   OnClick() {
-    console.log("wiek", this.age, "płeć" , this.gender, "waga", this.weight, "wzrost" , this.length, this.activity);
 
-    /*this.activity = this.activity.toNumber(this.activity);*/
+    if ( this.age < 1 || this.weight < 0 || this.length < 10)
+    {
+      if ( this.age < 1 )
+      {
+        this.ifAgeOk = false;
+        this.age = null;
+      } else {
+        this.ifAgeOk = true;
+      }
+
+      if ( this.weight < 1 )
+      {
+        this.ifWeightOk = false;
+        this.weight = null;
+      } else {
+        this.ifWeightOk = true;
+      }
+
+      if ( this.length < 10 )
+      {
+       this.ifLenghtOk = false;
+       this.length = null;
+      } else {
+        this.ifLenghtOk = true;
+      }
+      return;
+    }
+
+    this.ifAgeOk = true;
+    this.ifLenghtOk = true;
+    this.ifWeightOk = true;
+
     if (this.gender === '0')
     {
       this.BMR = 66 + (13.7 * this.weight) + (5 * this.length) - (6.76 * this.age);
@@ -36,10 +68,10 @@ export class BMRcalcComponent implements OnInit {
       this.BMR = ( 655 + (9.6 * this.weight) + (1.85 * this.length) - (4.7 * this.age)) * this.activity;
       this.BMR = this.BMR.toFixed();
     }
-    console.log("BMR wynosi", this.BMR, " kcal" );
 
 
     this.view = !this.view;
+
   }
 
   onClear() {
