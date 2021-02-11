2237233f52410b08bfa466af93d0d9d6e2917c2a
Jakub Faruga <kuba-faruga@o2.pl>
2021-02-12 00:47:46 +0100

    Dodanie suplementow .ts

---
+++ src/app/suplements/suplements.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SuplementsComponent } from './suplements.component';
+
+describe('SuplementsComponent', () => {
+  let component: SuplementsComponent;
+  let fixture: ComponentFixture<SuplementsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SuplementsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SuplementsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/suplements/suplements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suplements',
+  templateUrl: './suplements.component.html',
+  styleUrls: ['./suplements.component.css']
+})
+export class SuplementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
